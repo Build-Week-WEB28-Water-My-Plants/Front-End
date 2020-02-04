1ca7765120a4a8bfb915f10358edb4322454de2d
Alex Cooter <56034009+alex-lc@users.noreply.github.com>
2020-02-04 09:51:17 -0600

    working on refactoring login with useReducer for better state management

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useReducer, useEffect } from 'react';
 import styled from 'styled-components';
 import axios from 'axios';
 import { useHistory } from 'react-router-dom';
@@ -10,21 +10,46 @@ import { UserContext } from '../contexts';
 import View from '../assets/View.svg';
 
 // want to refactor all state into using useReducer with context API
-// function loginReducer(state, action) {
-//     switch (action.type) {
-//         default:
-//             return state;
-//     }
-// }
+function loginReducer(state, action) {
+    switch (action.type) {
+        case 'FIELD':
+            return {
+                ...state,
+                [action.field]: action.value
+            }
+        case 'START_LOGIN':
+            console.log(`hello from start login`);
+            return {
+                ...state,
+                isLoading: true
+            };
+        case 'LOGIN_SUCCESS':
+            console.log(`hello from login success`);
+            return {
+                ...state,
+                isLoading: false,
+                isLoggedIn: true
+            };
+        case 'ERROR':
+            console.log(`hello from error`);
+            return {
+                ...state,
+                isLoading: false,
+                error: 'error'
+            };
+        default:
+            return state;
+    }
+}
 
-// want to refactor all state into using useReducer with context API
-// const initialState = {
-//     username: '',
-//     password: '',
-//     isLoading: '',
-//     isLoggedIn: '',
-//     error: ''
-// }
+// initial state to handle user action on login form
+const initialState = {
+    username: '',
+    password: '',
+    isLoading: false,
+    isLoggedIn: false,
+    error: ''
+}
 
 function Login(props) {
 
@@ -34,84 +59,95 @@ function Login(props) {
     const { user, setUser, setIsLoading } = useContext(UserContext);
 
     // want to refactor all state into using useReducer with context API
-    // const [state, dispatch] = useReducer(loginReducer, initialState);
+    const [state, dispatch] = useReducer(loginReducer, initialState);
 
     // extra error state for login
-    const [error, setError] = useState({
-        status: false,
-        message: ''
-    })
+    // const [error, setError] = useState({
+    //     status: false,
+    //     message: ''
+    // })
 
     // handle login form input change
-    const handleChange = (e) => {
-        setUser({
-            ...user,
-            [e.target.name]: e.target.value
-        });
-    }
+    // const handleChange = (e) => {
+    //     setUser({
+    //         ...user,
+    //         [e.target.name]: e.target.value
+    //     });
+    // }
 
 
     // login function for form submit
     const log = (user) => {
-        setIsLoading(true);
+        // setIsLoading(true);
+        dispatch({ type: 'START_LOGIN', });
+        console.log(state);
+        // setUser({
+        //     ...user,
+        //     username: state.username,
+        //     password: state.password
+        // });
         axios.post(`https://water-my-plants-1.herokuapp.com/api/users/login`, user)
             .then((res) => {
                 console.log(res);
                 localStorage.setItem('token', res.data.token);
                 localStorage.setItem('id', res.data.id);
-                setIsLoading(false);
-                setError({
-                    status: false,
-                    message: ''
-                });
-                history.push(`/plants`);
+                dispatch({ type: 'LOGIN_SUCCESS' });
+
+                // setIsLoading(false);
+                // setError({
+                //     status: false,
+                //     message: ''
+                // });
+
+                // history.push(`/plants`);
             })
             .catch((err) => {
                 console.log(err.response.data.error);
-                setError({
-                    status: true,
-                    message: 'Incorrect login. Try again.'
-                });
-                setUser({
-                    ...user,
-                    username: '',
-                    password: ''
-                });
+                dispatch({ type: 'LOGIN_FAIL' });
+                // setError({
+                //     status: true,
+                //     message: 'Incorrect login. Try again.'
+                // });
+                // setUser({
+                //     ...user,
+                //     username: '',
+                //     password: ''
+                // });
             })
     }
 
     return (
         <FormContainer>
-
+            {/* {console.log(`this is our new user:`, user)} */}
             <div className="login-message">
                 <p>Please login to manage and view your plants.</p>
             </div>
 
             {/* incorrect login error message */}
-            {error.status && <p className="incorrect-login">{error.message}</p>}
+            {/* {error.status && <p className="incorrect-login">{error.message}</p>} */}
 
             <form onSubmit={(e) => {
                 e.preventDefault();
                 log(user);
-                setError({
-                    status: false,
-                    message: ''
-                });
+                // setError({
+                //     status: false,
+                //     message: ''
+                // });
             }}>
                 <input
                     type="text"
                     name="username"
                     placeholder="Username"
-                    value={user.username}
-                    onChange={handleChange}
+                    value={state.username}
+                    onChange={e => dispatch({ type: 'FIELD', field: 'username', value: e.currentTarget.value })}
                     autoComplete="off"
                 />
                 <input
                     type="password"
                     name="password"
                     placeholder="Password"
-                    value={user.password}
-                    onChange={handleChange}
+                    value={state.password}
+                    onChange={e => dispatch({ type: 'FIELD', field: 'password', value: e.currentTarget.value })}
                     autoComplete="off"
                 />
                 <button type="submit">Login</button>
@@ -130,129 +166,129 @@ function Login(props) {
 }
 
 const FormContainer = styled.div`
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-
+            width: 100%;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+        
     .svg-banner {
-        display: flex;
-        justify-content: center;
-
+                display: flex;
+            justify-content: center;
+    
         img {
-            width: 80%;
+                width: 80%;
         }
     }
 
     .incorrect-login {
-        margin-top: 3rem;
-        color: #872a26;
-        font-size: 1.4rem;
-    }
-
+                margin - top: 3rem;
+            color: #872a26;
+            font-size: 1.4rem;
+        }
+    
     span {
-        margin-top: 0.5rem;
-        font-size: 1.2rem;
-
+                margin - top: 0.5rem;
+            font-size: 1.2rem;
+    
         &:hover {
-            cursor: pointer;
+                cursor: pointer;
         }
     }
 
     .login-message {
-        h2 {
-            font-size: 3rem;
+                h2 {
+                font - size: 3rem;
             color: #5f7361;
         }
 
         p {
-            margin-top: 1rem;
+                margin - top: 1rem;
             font-size: 1.6rem;
             font-weight: 300;
             letter-spacing: 0.1rem;
             color: #444444;
 
             @media (max-width: 451px) {
-                font-size: 1.4rem;                
-            }
+                font - size: 1.4rem;
+        }
 
             @media (max-width: 403px) {
-                font-size: 1.2rem;                
-            }
+                font - size: 1.2rem;
         }
     }
+}
 
     form {
-        padding: 2.5rem 0;
-        display: flex;
-        justify-content: space-around;
-        align-items: center;
-        width: 60%;
-        margin-bottom: 5%;
-
+                padding: 2.5rem 0;
+            display: flex;
+            justify-content: space-around;
+            align-items: center;
+            width: 60%;
+            margin-bottom: 5%;
+    
         @media (max-width: 1440px) {
-            width: 70%;
+                width: 70%;
         }
 
         @media (max-width: 1245px) {
-            width: 80%;
+                width: 80%;
         }
 
         @media (max-width: 1085px) {
-            width: 90%;
+                width: 90%;
         }
-        
+
         @media (max-width: 965px) {
-            width: 100%;
+                width: 100%;
         }
 
         @media (max-width: 850px) {
-            flex-direction: column;
+                flex - direction: column;
         }
     }
 
     input {
-        margin: 0.5rem 0;
-        width: 20rem;
-        height: 3.5rem;
-        background: #bfbfbf;
-        border: none;
-        border-radius: 0.3rem;
-        padding: 0.5rem 0.5rem 0.5rem 1rem;
-        font-size: 1.2rem;
-        font-weight: 300;
-        letter-spacing: 0.1rem;
-
+                margin: 0.5rem 0;
+            width: 20rem;
+            height: 3.5rem;
+            background: #bfbfbf;
+            border: none;
+            border-radius: 0.3rem;
+            padding: 0.5rem 0.5rem 0.5rem 1rem;
+            font-size: 1.2rem;
+            font-weight: 300;
+            letter-spacing: 0.1rem;
+    
         &:focus {
-            outline: none;
+                outline: none;
             border: 1px solid #ababab;
         }
     }
 
     button {
-        width: 20rem;
-        height: 3.5rem;
-        margin: 1rem 0;
-        background: #d1ffd6;
-        border: none;
-        border-radius: 0.3rem;
-        transition: all 100ms;
-        box-shadow: 0px 2px 5px -5px;
-        letter-spacing: 0.1rem;
-
+                width: 20rem;
+            height: 3.5rem;
+            margin: 1rem 0;
+            background: #d1ffd6;
+            border: none;
+            border-radius: 0.3rem;
+            transition: all 100ms;
+            box-shadow: 0px 2px 5px -5px;
+            letter-spacing: 0.1rem;
+    
         &:hover {
-            transition: background 100ms;
+                transition: background 100ms;
             cursor: pointer;
             background: #afdeb4;
         }
     }
 
     .extra-options {
-        width: 30%;
-        display: flex;
-        justify-content: space-evenly;
-    }
-
-`;
+                width: 30%;
+            display: flex;
+            justify-content: space-evenly;
+        }
+    
+    `;
 
 export default Login;
